0ca711ca76a810a248e493bc32f8f731fa396fad
Fabien Darbellay <Fabien.Darbellay@ceff.ch>
2023-08-21 11:41:01 +0200

    ajoute du 2eme exercice

---
+++ exercice_2.php
@@ -0,0 +1,27 @@
+
+
+<html>
+<head>
+<title>Exemple2</title>
+</head>
+<body>
+<?php
+
+echo "<table border=\"2\">";
+echo "<tr>";
+       echo "<td>Nom</td>";
+       echo "<td>Prénom</td>";
+echo "</tr>";
+echo "<tr>";
+       echo "<td>Lerdorf</td>";
+       echo "<td>Rasmus</td>";
+echo "</tr>";
+echo "<tr>";
+       echo "<td>Computer</td>";
+       echo "<td>Jules</td>";
+echo "</tr>";
+echo "</table>";
+?>
+</body>
+</html>
+
