be2350948cb8acb5d7fa0983e792f2659860961e
Fabien Darbellay <Fabien.Darbellay@ceff.ch>
2023-08-21 13:37:51 +0200

    exercice 3 et création exercice 4

---
+++ exercice_3.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Exemple3</title>
+</head>
+<body>
+<?php
+$nom = "Darbellay";
+$prenom = "Fabien";
+$profession = "ict";
+echo "<b>".$nom.", ".$prenom."</b>".", ".$profession."<br>"
+?>
+</body>
+</html>
