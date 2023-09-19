ae6c98bbf10157d2896183e964164c311938f0f8
Fabien Darbellay <Fabien.Darbellay@ceff.ch>
2023-09-19 08:44:42 +0200

    travail sur exercice

--- jquery/ex14/mon-script.js
+++ jquery/ex14/mon-script.js
@@ -1,8 +1,9 @@
 $(function (){
     $('a').tooltip({
-        content: "<strong>reote le mano</strong>",
+        content: "<strong>" + $('a').on('mouseover', function (){
+            return $(this).attr('data-desc');
+        }) + "</strong>",
         show: { effect: "explode", duration: 800 },
         tooltipClass: "custom-tooltip-styling",
-        position:{collision:'fit'}
     });
 });
