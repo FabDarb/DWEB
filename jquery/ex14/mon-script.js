529d4b551871c775ab04214bf0e4f8750ba53d03
Fabien Darbellay <Fabien.Darbellay@ceff.ch>
2023-09-22 07:37:03 +0200

    exercice plus

--- jquery/ex14/mon-script.js
+++ jquery/ex14/mon-script.js
@@ -1,9 +1,12 @@
 $(function (){
     $('a').tooltip({
-        content: "<strong>" + $('a').on('mouseover', function (){
-            return $(this).attr('data-desc');
-        }) + "</strong>",
-        show: { effect: "explode", duration: 800 },
+        item: "a",
+        content: function (){
+            return "<strong>" + $(this).attr('data-desc') +"</strong>" +
+                "<img alt='t' src='../images/" + $(this).attr('data-dif') + ".png' /><br>" +
+                "<strong>" + $(this).attr('href').split('/')[2].split('.')[0] +"</strong>"
+        },
+        show: { effect: "size", duration: 1000 },
         tooltipClass: "custom-tooltip-styling",
     });
 });
